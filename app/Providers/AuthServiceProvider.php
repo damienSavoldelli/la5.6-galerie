1b4a8732e1cc7e8adbed6cc5ffc9677b9d9032ca
damien <damien.savoldelli@gmail.com>
2018-02-20 23:12:23 +0100

    update AuthServiceProvider, call routes pasport in boot

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Laravel\Passport\Passport;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -30,6 +31,6 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Passport::routes(); // boot passport routes for API
     }
 }
