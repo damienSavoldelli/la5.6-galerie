7d6f18d5d0db0f9e19e3b43b69d7ca4a8272fd99
damien <damien.savoldelli@gmail.com>
2018-03-08 04:18:00 +0100

    update auth service provider, add token expiration : - token : 15 days - refresh : 30 days

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Laravel\Passport\Passport;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Carbon;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 use App\Policies\{ImagePolicy, UserPolicy};
@@ -32,5 +33,9 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Passport::routes(); // boot passport routes for API
+
+        Passport::tokensExpireIn(Carbon::now()->addDays(15));
+
+        Passport::refreshTokensExpireIn(Carbon::now()->addDays(30));
     }
 }
